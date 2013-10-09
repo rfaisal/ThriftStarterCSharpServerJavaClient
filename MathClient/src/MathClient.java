d3983d0e9d7ee78c8b3daf73c270718687916cec
Faisal Rahman <md.faisal.rahman@gmail.com>
2013-10-09 16:18:35 -0400

    commit for release

--- MathClient/src/MathClient.java
+++ MathClient/src/MathClient.java
@@ -7,7 +7,6 @@ public class MathClient {
 	 public static void main(String[] args) throws TException{
 		 TTransport transport = new TSocket("localhost", 9095);
 		 transport.open();
-
 		 TProtocol protocol = new  TBinaryProtocol(transport);
 		 MathService.Client client = new MathService.Client(protocol);
 		 System.out.println(client.add(10, 20));
