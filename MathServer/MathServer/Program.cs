3761ae9c5b881da1d423a8561088588d44fd174b
Faisal Rahman <md.faisal.rahman@gmail.com>
2013-10-09 15:20:53 -0400

    C# Server finished

--- MathServer/MathServer/Program.cs
+++ MathServer/MathServer/Program.cs
@@ -2,13 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Thrift.Server;
+using Thrift.Transport;
 
-namespace MathServer
+namespace Math
 {
     class Program
     {
         static void Main(string[] args)
         {
+            try
+            {
+                MathServer handler = new MathServer();
+                MathService.Processor processor = new MathService.Processor(handler);
+                TServerTransport serverTransport = new TServerSocket(9095);
+                TServer server = new TSimpleServer(processor, serverTransport);
+                Console.WriteLine("Starting the server...");
+                server.Serve();
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine(x.StackTrace);
+            }
+            Console.WriteLine("done.");
         }
     }
 }
